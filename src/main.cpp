2f878fdd4e9c51dde8ef2460a2cfdc0327fadb02
Ayush <iiser.ayush@gmail.com>
2026-02-11 21:47:22 +0530

    feat: Implement BMP file reading and writing
    This commit introduces the core functionality for reading and writing 24-bit BMP files.
    - Reads BMP file header and info header.
    - Validates file type and bit depth (currently only 24-bit supported).
    - Extracts pixel data and handles row padding.
    - Writes a copy of the processed BMP data to .

--- src/main.cpp
+++ src/main.cpp
@@ -24,38 +24,66 @@ int main(int argc, char* argv[]){
   //TODO: read the bmp file
   BMPFileHeader fileHeader;
 
+  BMPInfoHeader infoHeader;
+
   file.read(reinterpret_cast<char*>(&fileHeader),sizeof(fileHeader));
   
+  file.read(reinterpret_cast<char*>(&infoHeader),sizeof(infoHeader));
+
+  //validation
   if(fileHeader.file_type!=0x4D42){
     cerr<<"Error. File not a BMP. (magic number mismatch)"<<endl;
     return 1;
   }
 
-  //read the info header 
-  BMPInfoHeader infoHeader;
-
-  file.read(reinterpret_cast<char*>(&infoHeader),sizeof(infoHeader));
-
-  //Diagnosis printing
-  cout<<"---------------------------------------"<<endl;
-  cout<<"File Size: "<<fileHeader.file_size<<" bytes"<<endl;
-  cout<<"Dimensions: "<<infoHeader.width<<"x"<<infoHeader.height<<endl;
-  cout<<"Bit Depth: "<<infoHeader.bit_count<<"-bit"<<endl;
-  cout<<"Data Offset: "<<fileHeader.offset_data<<" bytes"<<endl;
-  cout<<"---------------------------------------"<<endl;
-  
-  //validation
   if(infoHeader.bit_count!=24){
     cerr<<"Error: TCS currently supports only 24-bit BMPs"<<endl;
     return 1;
   }
 
-  if(infoHeader.compression !=0){
-    cerr<<"Error: Compressed BPMs are not supported yet"<<endl;
-    return 1;
-  }
+  //preparing for pixel Data
+  int width = infoHeader.width;
+  int height = infoHeader.height;
+
+  //calculating padding
+  int padding = (4-(width*3)%4)%4;
   
-  cout<<"SUCCESS: valid 24-bit BMP detected "<<endl;
+  cout<<"Reading Image: "<<width<<"x"<<"height"<<endl;
+  cout<<"Padding per row :"<<padding<<" bytes"<<endl;
+
+  //moving cursor or pointer to start of pixel data
+  file.seekg(fileHeader.offset_data,file.beg);
+
+  vector<Pixel> pixelData(width*height);
+
+  //sara pixel data ko capture karen janab
+  for(int y=0;y<height;y++){
+    for(int x=0;x<width;x++){
+        file.read(reinterpret_cast<char*>(&pixelData[y*width +x]),3);
+    }
+    file.seekg(padding,ios::cur);
+  }
   file.close();
+
+  cout<<"Image Loaded Successfully"<<endl;
+  
+  //these are now testing methods
+  ofstream outFile("test_copy.bmp",ios::binary);
+
+  outFile.write(reinterpret_cast<const char*>(&fileHeader),sizeof(fileHeader));
+  outFile.write(reinterpret_cast<const char*>(&infoHeader),sizeof(infoHeader));
+
+  //pixel aur padding dalen
+  for(int y=0;y<height;y++){
+    for(int x=0;x<width;x++){
+      outFile.write(reinterpret_cast<const char*>(&pixelData[y*width+x]),3);
+    }
+
+    char pad[3] = {0,0,0};
+    outFile.write(pad,padding);
+  }
+
+  outFile.close();
+  cout<<"Success ! Wrote 'test_copy.bmp'"<<endl;
   return 0;
 }
