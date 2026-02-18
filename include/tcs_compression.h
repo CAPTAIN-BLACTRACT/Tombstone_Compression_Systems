465c23348c1042f017e722fe4a07dcd4e623983e
Ayush <iiser.ayush@gmail.com>
2026-02-18 22:37:18 +0530

    ft: completed the decompression logic (maybe broken)

--- include/tcs_compression.h
+++ include/tcs_compression.h
@@ -3,6 +3,7 @@
 
 #include <cstddef>
 #include <cstdint>
+#include <memory>
 #include <string.h>
 #include <vector>
 #include <fstream>
@@ -22,10 +23,9 @@ void decompressRLE(string tcsFilename, string outBmpName){
   std::cerr<<"Cant open the file "<<tcsFilename<<std::endl;
     return;
   }
-  std::ofstream outFile(outBmpName,std::ios::binary);
   
   char magicBuffer[5]={0};
-  inputFile.read(&magicBuffer, 4);
+  inputFile.read(magicBuffer, 4);
 
   if(std::string(magicBuffer)!="TCS1"){  
     std::cerr<<"This is not a valid tcs file"<<std::endl;
@@ -38,7 +38,70 @@ void decompressRLE(string tcsFilename, string outBmpName){
   inputFile.read(reinterpret_cast<char*>(&height), sizeof(height));
 
   std::cout<<"Restoring image: "<<width<<"x"<<height<<std::endl;
+  int padding = (4-(width*3)%4)%4;
+
+  uint32_t imageSize = (width*3 + padding)*height;
+  uint32_t fileSize = 54 + imageSize;
+// bhai ai ki madd se bna lo header aur info file 
+  // apna dimag mt lagaie
   
+}
+  BMPFileHeader fileHeader;
+  fileHeader.file_type = 0x4D42;
+  fileHeader.file_size = fileSize;
+  fileHeader.reserved1 = 0;
+  fileHeader.reserved2 = 0;
+  fileHeader.offset_data = 54;
+
+  BMPInfoHeader infoHeader;
+  infoHeader.size = 40;
+  infoHeader.width = width;
+  infoHeader.height = height;
+  infoHeader.planes = 1;
+  infoHeader.bit_count = 24;
+  infoHeader.compression = 0;
+  infoHeader.size_image = imageSize;
+  infoHeader.x_pixels_per_meter = 0;
+  infoHeader.y_pixels_per_meter = 0;
+  infoHeader.colors_used = 0;
+  infoHeader.colors_important = 0;
+//ab likh lo
+std::ofstream outFile(outBmpName,std::ios::binary);
+
+outFile.write(reinterpret_cast<const char*>(&fileHeader),sizeof(fileHeader));
+outFile.write(reinterpret_cast<const char*>(&infoHeader),sizeof(infoHeader));
+
+int currentX =0;
+
+while (inputFile.peek()!=EOF) {
+  uint8_t count;
+  Pixel color;
+
+  inputFile.read(reinterpret_cast<char*>(&count),1);
+  inputFile.read(reinterpret_cast<char*>(&color),3);
+
+//agar beech me file khatam ho jae to bhag jao
+  if(!inputFile) break;
+
+  //pixel ko count baar likho
+  for(int i=0;i<count;i++){
+    outFile.write(reinterpret_cast<const char*>(&color),3);
+    currentX++;
+
+    if(currentX==width){
+      char pad[3] = {0,0,0};
+      outFile.write(pad,padding);
+      currentX=0;
+    }
+  }
+
+
+
+}
+
+outFile.close();
+std::cout<<"Success! Decompressed to : "<<outBmpName<<std::endl;
+
 }
 
 void compressRLE(const std::vector<Pixel>& pixels , int width, int height, const std::string& filename){
